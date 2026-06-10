f1ba7b60b2638a19eff47e1de478479072b13e75
Darsh Chaudhary <darsh.chaudhary@predusk.local>
2026-06-10 14:17:32 +0530

    feat(models): add task and user models

---
+++ task_management/models.py
@@ -0,0 +1,28 @@
+from enum import Enum
+from core.database import Base
+from sqlalchemy import Column, Integer, String, Enum, ForeignKey, DateTime, func
+
+class UserRole(str, Enum):
+    ADMIN = "admin"
+    MEMBER = "member"
+
+class TaskStatus(str, Enum):
+    PENDING = "pending"
+    IN_PROGRESS = "in_progress"
+    COMPLETED = "completed"
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    role = Column(Enum(UserRole), nullable=False)
+
+class Task(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    status = Column(Enum(TaskStatus), nullable=False)
+    assigned_to = Column(Integer, ForeignKey("users.id"), nullable=False)
+    created_at = Column(DateTime(timezone=False), server_default=func.now(), nullable=False)
+
