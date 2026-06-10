b7bfc7f0517d15e46feffb5716675d4fd3b3a32c
Darsh Chaudhary <darsh.chaudhary@predusk.local>
2026-06-10 12:57:55 +0530

    chore: scaffold standalone task management FastAPI app

---
+++ core/database.py
@@ -0,0 +1,15 @@
+from settings import DATABASE_URL;
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession;
+from sqlalchemy.orm import declarative_base;
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+SessionLocal = async_sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
+
+Base = declarative_base()
+
+async def get_db_session():
+    async with SessionLocal() as session:
+        yield session
---
+++ core/settings.py
@@ -0,0 +1,3 @@
+from settings import DATABASE_URL as db_url;
+
+DATABASE_URL=db_url;
